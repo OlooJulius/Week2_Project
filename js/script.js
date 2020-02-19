2cc1c66278ea7093e24c19823ff4d4c8c97ccb09
Oloo Julius <juliusolooolero@gmail.com>
2020-02-19 05:40:49 +0300

    itwill add const maleAkanNames and femaleAkanNames

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Declaring variable name ordered from Sunday
+
+const maleAkanNames = [
+    "Kwame",
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi"
+  ];
+  const femaleAkanNames = [
+    "Ama",
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua"
+  ];
